fd94633679a944385445d8b0a5acb26246f249fa
RostilKant <rostilnytskiy@gmail.com>
2020-02-13 17:31:51 +0200

    Applying Filtering

--- Entities/RequestFeatures/RequestParametrs.cs
+++ Entities/RequestFeatures/RequestParametrs.cs
@@ -16,6 +16,10 @@ namespace Entities
 
     public class EmployeeParameters : RequestParameters
     {
+        public uint MinAge { get; set; }
+        public uint MaxAge { get; set; } = uint.MaxValue;
+
+        public bool ValidAgeRange => MinAge < MaxAge;
         
     }
 }
